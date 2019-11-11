223d647c643c207e19065ad8ad7dbcd40c3a97fb
Pavel Martynov <pavlom@MacBook-Pro-Pavel.local>
2019-11-11 15:34:58 +0300

    ra hw router-menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePage from './components/HomePage'
+import DriftPage from './components/DriftPage'
+import ForzaPage from './components/ForzaPage'
+import TimeAttackPage from './components/TimeAttackPage'
+import Menu from './components/Menu'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Menu />
+        <div className="page">
+          <Route path="/" exact component={HomePage} />
+          <Route path="/drift" component={DriftPage} />
+          <Route path="/timeattack" component={TimeAttackPage} />
+          <Route path="/forza" component={ForzaPage} />
+        </div>
+      </div>
+    </Router>
   );
 }
 
